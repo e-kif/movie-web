775c370215cc19aa0c93693ae98b5203f962fecc
e-kif <e.kifiuk@gmail.com>
2024-10-04 22:10:25 +0300

    abstract method for crud were added to abstract parent class

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import os
 from data.sqlite_data_manager import SQLiteDataManager, db
 
 app = Flask(__name__)
-database_file = os.path.join(os.getcwd(), 'data', 'movies.sqlite')
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{database_file}'
+database_filename = os.path.join(os.getcwd(), 'data', 'movies.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{database_filename}'
 db.init_app(app)
 # with app.app_context():
 #     db.create_all()
-data = SQLiteDataManager(database_file)
+data = SQLiteDataManager(database_filename)
 
 
 if __name__ == '__main__':
--- data/data_manager.py
+++ data/data_manager.py
@@ -10,3 +10,19 @@ class DataManagerInterface(ABC):
     @abstractmethod
     def get_user_movies(self, user_id):
         pass
+
+    @abstractmethod
+    def add_user(self, user):
+        pass
+
+    @abstractmethod
+    def add_movie(self, title, director, year, rating):
+        pass
+
+    @abstractmethod
+    def update_movie(self, movie_id, title, director, year, rating):
+        pass
+
+    @abstractmethod
+    def delete_movie(self, movie_id):
+        pass
