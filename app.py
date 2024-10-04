5305a58a002006a40df993863b2e43c6285ed1fe
e-kif <e.kifiuk@gmail.com>
2024-10-04 22:05:15 +0300

    crud methods were added

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, redirect, jsonify
+import os
+from data.sqlite_data_manager import SQLiteDataManager, db
 
 app = Flask(__name__)
+database_file = os.path.join(os.getcwd(), 'data', 'movies.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{database_file}'
+db.init_app(app)
+# with app.app_context():
+#     db.create_all()
+data = SQLiteDataManager(database_file)
 
 
 if __name__ == '__main__':
--- data/sqlite_data_manager.py
+++ data/sqlite_data_manager.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from data_manager import DataManagerInterface
+from data.data_manager import DataManagerInterface
 
 db = SQLAlchemy()
 
@@ -13,11 +13,42 @@ class SQLiteDataManager(DataManagerInterface):
         return self.db.session.query(Users).all
 
     def get_user_movies(self, user_id):
-        pass
+        return self.db.session.query(Movies).join(UserMovies).filter(UserMovies.user_id == user_id)
+
+    def add_user(self, user):
+        self.db.session.add(Users(name=user))
+        self.db.session.commit()
+        return f'User {user} was added successfully.'
+
+    def add_movie(self, title, director, year, rating):
+        movie = Movies(
+            title=title,
+            director=director,
+            year=year,
+            rating=rating
+        )
+        self.db.session.add(movie)
+        self.db.session.commit()
+        return f'Movie "{title}" was added successfully.'
+
+    def update_movie(self, movie_id, title, director, year, rating):
+        the_movie = self.db.session.query(Movies).filter(Movies.id == movie_id).one()
+        the_movie.title = title
+        the_movie.director = director
+        the_movie.year = year
+        the_movie.rating = rating
+        self.db.session.commit()
+        return f'The movie "{title}" was update successfully.'
+
+    def delete_movie(self, movie_id):
+        the_movie = self.db.session.query(Movies).filter(Movies.id == movie_id).one()
+        title = the_movie.title
+        the_movie.delete()
+        self.db.session.commit()
+        return f'The movie "{title}" was deleted successfully.'
 
 
 class Users(db.Model):
-
     id: db.Mapped[int] = db.mapped_column(primary_key=True, autoincrement=True)
     name: db.Mapped[str] = db.mapped_column()
 
@@ -26,7 +57,6 @@ class Users(db.Model):
 
 
 class Movies(db.Model):
-
     id: db.Mapped[int] = db.mapped_column(primary_key=True, autoincrement=True)
     title: db.Mapped[str] = db.mapped_column()
     director: db.Mapped[str] = db.mapped_column()
@@ -41,10 +71,9 @@ class Movies(db.Model):
 
 
 class UserMovies(db.Model):
-
-    movie_id: db.Mapped[int] = db.mapped_column(db.ForeignKey('movies.id'))
-    user_id: db.Mapped[int] = db.mapped_column(db.ForeignKey('users.id'))
+    movie_id: db.Mapped[int] = db.mapped_column(db.ForeignKey('movies.id'), primary_key=True)
+    user_id: db.Mapped[int] = db.mapped_column(db.ForeignKey('users.id'), primary_key=True)
 
     def __repr__(self):
-        return (f'user={db.session.query(Users).filter(Users.id == self.user_id).one().name} '
-                f'movie={db.session.query(Movies).filter(Movies.id == self.movie_id).one().title}')
+        return (f'user={db.session.query(self.Users).filter(self.Users.id == self.user_id).one().name} '
+                f'movie={db.session.query(self.Movies).filter(self.Movies.id == self.movie_id).one().title}')
